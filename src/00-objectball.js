2cf2651913d16325a29c02379dce216aeb6abbd6
+ <+>
2024-07-16 14:38:32 -0500

    complete lab

---
+++ src/00-objectball.js
@@ -0,0 +1,12 @@
+function gameObject() {
+    const gameObj = {};
+    gameObj.home = {};
+    gameObj.away = {};
+    gameObj.home.teamName = "Brooklyn Nets";
+    gameObj.home.colors = ["Black", "White"];
+    gameObj.home.players = ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"];
+    gameObj.away.teamName = "Charlotte Hornets";
+    gameObj.away.colors = ["Turquoise", "Purple"];
+    gameObj.away.player = ["Jeff Adrien", "Bismak Biyombo", "DeSagna Diop", "Ben Gordon", "Brendan Haywood"];
+    return gameObj;
+}
